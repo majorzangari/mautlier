88b1ee4b60d4e50bde0942ef7c7aa3d8c34cd6fe
Major Zangari <maz76@cornell.edu>
2025-04-24 10:36:18 -0400

    pawn capture generation

--- state/move.c
+++ state/move.c
@@ -11,9 +11,9 @@
 #define lsb_index(bb) (__builtin_ctzll(bb))
 #define pop_lsb(bb) ((bb) &= (bb) - 1)
 
-inline Move encode_move(int from_square, int to_square, int flags) {
+Move encode_move(int from_square, int to_square, int flags) {
   return (from_square << 10) | (to_square << 4) | flags;
-}
+} // TODO should probably inline this (or macro? idk)
 
 int generate_pawn_pushes(Bitboard pawns, Bitboard occupied, Move *moves,
                          ToMove color) {
@@ -30,8 +30,8 @@ int generate_pawn_pushes(Bitboard pawns, Bitboard occupied, Move *moves,
   while (temp_single_dest) {
     int to_square = lsb_index(temp_single_dest);
     int from_square = (color == WHITE) ? to_square + 8 : to_square - 8;
-    *moves++ = (to_square << 10) |
-               (from_square << 4); // TODO: add other flags, promotion
+    *moves++ = encode_move(from_square, to_square,
+                           0); // TODO: add other flags, promotion
     num_moves++;
     pop_lsb(temp_single_dest);
   }
@@ -40,15 +40,45 @@ int generate_pawn_pushes(Bitboard pawns, Bitboard occupied, Move *moves,
   Bitboard double_push_dest =
       (color == WHITE) ? ((single_push_dest << 8) & empty & double_rank)
                        : ((single_push_dest >> 8) & empty & double_rank);
-  printf("double_push_dest: %lx\n", double_push_dest);
   while (double_push_dest) {
     int to_square = lsb_index(double_push_dest);
     int from_square = (color == WHITE) ? to_square + 16 : to_square - 16;
-    *moves++ =
-        (to_square << 10) |
-        (from_square << 4); // TODO add other flags (might not be necessary)
+    *moves++ = encode_move(from_square, to_square,
+                           0); // TODO: add other flags (might not be necessary)
     num_moves++;
     pop_lsb(double_push_dest);
+  }
+
+  return num_moves;
+}
+
+int generate_pawn_captures(Bitboard pawns, Bitboard enemy_occupied, Move *moves,
+                           ToMove color) {
+  int num_moves = 0;
+
+  Bitboard left_capture = (color == WHITE)
+                              ? (pawns << 9) & enemy_occupied & ~FILE_H
+                              : (pawns >> 7) & enemy_occupied & ~FILE_H;
+  Bitboard right_capture = (color == WHITE)
+                               ? (pawns << 7) & enemy_occupied & ~FILE_A
+                               : (pawns >> 9) & enemy_occupied & ~FILE_A;
+
+  while (left_capture) {
+    int to_square = lsb_index(left_capture);
+    int from_square = (color == WHITE) ? to_square - 9 : to_square + 7;
+    *moves++ = encode_move(from_square, to_square,
+                           1); // TODO: add other flags CAPTURE
+    num_moves++;
+    pop_lsb(left_capture);
+  }
+
+  while (right_capture) {
+    int to_square = lsb_index(right_capture);
+    int from_square = (color == WHITE) ? to_square - 7 : to_square + 9;
+    *moves++ = encode_move(from_square, to_square,
+                           1); // TODO: add other flags CAPTURE
+    num_moves++;
+    pop_lsb(right_capture);
   }
 
   return num_moves;
@@ -68,7 +98,9 @@ int generate_knight_moves(Bitboard knights, Bitboard same_side_occupied,
     while (pseudo_moves) {
       int to_square = lsb_index(pseudo_moves);
       pop_lsb(pseudo_moves);
-      *moves++ = (index << 10) | (to_square << 4); // TODO: add other flags
+      *moves++ =
+          encode_move(index, to_square,
+                      0); // TODO: add other flags (might not be necessary)
       num_moves++;
     }
   }
@@ -82,9 +114,12 @@ int generate_moves(Board *board, Move moves[MAX_MOVES]) {
   Bitboard pawns = board->pieces[board->to_move][PAWN];
   Bitboard knights = board->pieces[board->to_move][KNIGHT];
   Bitboard same_side_occupied = board->occupied_by_color[board->to_move];
+  Bitboard enemy_side_occupied = board->occupied_by_color[1 - board->to_move];
   move_count += generate_knight_moves(knights, same_side_occupied, moves);
   move_count += generate_pawn_pushes(pawns, board->occupied, moves + move_count,
                                      board->to_move);
-  *(moves + move_count) = (uint16_t)0;
+  move_count += generate_pawn_captures(pawns, enemy_side_occupied,
+                                       moves + move_count, board->to_move);
+  *(moves + move_count) = (uint16_t)0; // TEMP
   return move_count;
 }
