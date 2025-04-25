31f7f7de5a893f447364145f606a6cac0296d707
Major Zangari <maz76@cornell.edu>
2025-04-25 09:06:46 -0400

    Refactor constant generation to source file

---
+++ state/constants.h
@@ -0,0 +1,9 @@
+#ifndef MAUTLIER_CONSTANTS_H
+#define MAUTLIER_CONSTANTS_H
+
+#include "board.h"
+
+extern const Bitboard knight_moves[64];
+extern const Bitboard king_moves[64];
+
+#endif // MAUTLIER_CONSTANTS_H
--- state/move.h
+++ state/move.h
@@ -12,6 +12,21 @@
 #define lsb_index(bb) (__builtin_ctzll(bb))
 #define pop_lsb(bb) ((bb) &= (bb) - 1)
 
+#define FLAGS_NONE 0
+#define FLAGS_DOUBLE_PUSH 1
+#define FLAGS_SHORT_CASTLE 2
+#define FLAGS_LONG_CASTLE 3
+#define FLAGS_PROMOTION 4
+#define FLAGS_KNIGHT_PROMOTION 4
+#define FLAGS_BISHOP_PROMOTION 5
+#define FLAGS_ROOK_PROMOTION 6
+#define FLAGS_QUEEN_PROMOTION 7
+#define FLAGS_EN_PASSANT 9
+
+// only flag that should be mixed with other flags
+// all other flags should be used alone
+#define FLAGS_CAPTURE 8
+
 typedef uint16_t Move;
 
 int generate_moves(Board *board, Move moves[MAX_MOVES]);
